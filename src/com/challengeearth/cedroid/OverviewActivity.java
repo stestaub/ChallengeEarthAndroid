651a409220fc8cbf7ad94af6210b781497f001e6
Stefan Staub <stub@zhaw.ch>
2012-01-11 11:42:44 +0100

    added image view using a view binder

--- src/com/challengeearth/cedroid/OverviewActivity.java
+++ src/com/challengeearth/cedroid/OverviewActivity.java
@@ -1,14 +1,21 @@
 package com.challengeearth.cedroid;
 
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
 
 import com.challengeearth.cedroid.ChallengeData.DbHelper;
+import com.challengeearth.cedroid.helpers.AdapterImageLoader;
 
 public class OverviewActivity extends Activity {
     DbHelper dbHelper;
@@ -17,8 +24,10 @@ public class OverviewActivity extends Activity {
     Cursor cursor;
     SimpleCursorAdapter adapter;
     
-    static final String[] MAP_FROM = {ChallengeData.C_TITLE, ChallengeData.C_DESC};
-    static final int[] MAP_TO = {R.id.title, R.id.description};
+    private static final String TAG = "OverviewActivity";
+    
+    static final String[] MAP_FROM = {ChallengeData.C_TITLE, ChallengeData.C_DESC, ChallengeData.C_IMAGE};
+    static final int[] MAP_TO = {R.id.title, R.id.description, R.id.image};
 	
 	/** Called when the activity is first created. */
     @Override
@@ -32,6 +41,7 @@ public class OverviewActivity extends Activity {
         dbHelper = new DbHelper(this);
         db = dbHelper.getReadableDatabase();
         
+       
     }
 
 	@Override
@@ -46,8 +56,26 @@ public class OverviewActivity extends Activity {
 		cursor = db.query(ChallengeData.TABLE, null, null, null, null, null, null);
 		startManagingCursor(cursor);
 		adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, MAP_FROM, MAP_TO);
+		adapter.setViewBinder(VIEW_BINDER);
 		challengeList.setAdapter(adapter);
 	}
     
-    
+    static final ViewBinder VIEW_BINDER = new ViewBinder() {
+		
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if(view.getId() != R.id.image) {
+				return false;
+			}
+			ImageView image = (ImageView)view;
+			AdapterImageLoader imageLoader = new AdapterImageLoader(null);
+			try {
+				imageLoader.addImage(new URL(cursor.getString(columnIndex)), image);
+			}
+			catch (Exception e) {
+				Log.e(TAG, "could not load image", e);
+			}
+			return true;
+		}
+	};
 }
