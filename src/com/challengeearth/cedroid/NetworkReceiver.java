d3c33f58c9f4df397ded1a7af9023c604778ccf8
Stefan Staub <stub@zhaw.ch>
2012-01-11 14:14:47 +0100

    added network listener to start or stop updater on network changes

---
+++ src/com/challengeearth/cedroid/NetworkReceiver.java
@@ -0,0 +1,27 @@
+package com.challengeearth.cedroid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+public class NetworkReceiver extends BroadcastReceiver {
+
+	private static final String TAG = "NetworkReceiver";
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		boolean isNetworkDown = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+		
+		if(isNetworkDown) {
+			Log.d(TAG, "No internet connection. Stoping Updater Service");
+			context.stopService(new Intent(context, UpdateService.class));
+		}
+		else {
+			Log.d(TAG, "internet connection available, starting updater service");
+			context.startService(new Intent(context, UpdateService.class));
+		}
+	}
+
+}
